3d0e99b41bb75a1bd66a0bcac59e1aef25811d4f
Dev Sonone <devesonone222@gmail.com>
2025-12-25 01:24:54 -0800

    learn router

---
+++ reactRouter/src/Components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function About() {
+  return (
+      <div className="py-16 bg-white">
+          <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+              <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                  <div className="md:5/12 lg:w-5/12">
+                      <img
+                          src="https://imgs.search.brave.com/Pn_ZeyCS5AxWXYDsKYsTvo3VOKoZkLsc8WE7gudejPk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMjQv/Njc0Lzc0Mi9zbWFs/bC9jdXRlLXNwaWRl/ci1zdXBlcmhlcm8t/c3dpbmdpbmctaW4t/dGhlLWNpdHktZnJl/ZS12ZWN0b3IuanBn"
+                          alt="image"
+                      />
+                  </div>
+                  <div className="md:7/12 lg:w-6/12">
+                      <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+Download Epic Spider-Man Wallpapers in Stunning HD & 4K                      </h2>
+                      <p className="mt-6 text-gray-600">
+                          Step into the Spider-Verse with our premium collection of Spider-Man wallpapers designed for true fans. Discover iconic poses, intense action scenes, and artistic designs inspired by comics, movies, and games. Every wallpaper is carefully selected to deliver sharp details and vibrant colors.
+                      </p>
+                      <p className="mt-4 text-gray-600">
+                          All wallpapers are available in HD, Full HD, and 4K resolutions, perfect for mobile, desktop, and widescreen displays. Enjoy fast, free downloads with no quality loss and easy access. Transform your screen into a heroic masterpiece with Spider-Man today.
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
+}
